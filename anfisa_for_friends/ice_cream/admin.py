c1b45ea138a0d7a5dd3f413e79b18c341f1b2426
Roman Barmin <roma.barmin.06@gmail.com>
2025-12-10 16:04:33 +0300

    fixtures + superuser 'wertgot' + register models for admin zone

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(Category)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+admin.site.register(IceCream)
