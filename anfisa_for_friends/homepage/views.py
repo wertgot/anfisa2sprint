a09b9eaa747e356c55ea98838219f9c48affa02c
Chamomile <barmin.roma.06@gmail.com>
2025-12-15 11:04:04 +0300

    обновил requirements + fiter выборки данных из бд

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -6,7 +6,9 @@ from ice_cream.models import IceCream
 def index(request):
     template = 'homepage/index.html'
     # Запишите в переменную ice_cream_list новый QuerySet
-    ice_cream_list = IceCream.objects.values('id', 'title', 'description')
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+    ).filter(is_on_main=True, is_published=True)
     context = {
         'ice_cream_list': ice_cream_list,
     }
